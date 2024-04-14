5444d10ab9ff6528d18ca913ef02d272cc6f180b
sadaqatbarkat <153889463+sadaqatbarkat@users.noreply.github.com>
2024-04-14 19:28:47 +0500

    add index and app.js file

---
+++ app.js
@@ -0,0 +1,129 @@
+//Q1
+// let firstName = prompt("Enter the first Name");
+// let lastName = prompt("Enter the last Name");
+// let fullName = firstName + " "  + lastName
+
+// document.write("Asslam-o-alikum! " + fullName)
+
+
+//Q2
+//let favouriteMobile = prompt("What is your favourite mobile phone model")
+
+//document.write("My favourite phone is: "+ favouriteMobile  + "<br>" +" Lenght of String: "+ favouriteMobile.length)
+
+//Q3
+// let coutry = "Pakistani";
+
+// document.write("String: "+ coutry +"<br>" +"Index of 'n':  "  +coutry.indexOf("n") );
+
+
+// Q4
+//let world = "Hello World";
+//document.write("String: Hello World" +"<br>" + "Last index of 'l': " + world.lastIndexOf("l"));
+
+
+//Q5
+//let characterPakstani = "Pakistani";
+//document.write("String: Pakistani"+ "<br>"+ "Character at index "+characterPakstani.indexOf("i") +": "  +characterPakstani.charAt(3))
+
+// Q6
+// let first_name =prompt("Enter the first name")
+// let last_name =prompt("Enter the first name")
+// let full_name = first_name.concat(last_name)
+// document.write(full_name)
+
+
+//Q7
+// let city = "Hyderabad".replace("Hyder","Islam");
+// document.write(city)
+
+// Q8
+// let message = "Ali and Sami are best friends.They play cricket and football toghether."
+// let replace = message.replaceAll("and","&")
+// document.write(replace)
+
+
+// Q9
+// let str = "472"
+// document.write("Value: "+str +"<br>"  +"Type:" + typeof(str) +"<br>"  )
+// let conversion = Number(str)
+// document.write("Value: "+conversion + "<br>"+"Type: "   + typeof(conversion))
+
+// Q10
+// let userInput = prompt("Enter any Word/Letter");
+// document.write("User input: "+userInput + "<br>"  + "<br>")
+
+// let upperCase = userInput.toUpperCase()
+// document.write("Upper Case:"+ upperCase)
+
+
+// let language = prompt("Enter a word/letter")
+// document.write("User input: "+ language  + "<br>")
+// let convert = language.slice(0,1).toUpperCase() + language.slice(1).toLowerCase()
+
+// document.write("Title Case: "+ convert)
+
+
+// Q12
+
+// let num = 35.36;
+// document.write("Number: "+ num)
+// num =  35.36
+// let num2 = Math.ceil(num)
+// document.write(num2)
+//Q13
+
+// let a= 455.4;
+// document.write(a.toFixed(2))
+
+
+
+
+// Q14
+// let A = ["cake", "apple pie", "cookie", "chips", "patties"]
+
+// let userSearch = prompt("Welcome to ABC Bakery.What do you want to order sir/ma'am?")
+// let flag= false
+// for(let b = 0;b<A.length;b++){
+// 	if(userSearch===A[b]){
+// 		flag = true;
+// 		document.write(userSearch  +"<b>" +" is available"  +"</b>"+ " at index "+ A.indexOf(userSearch) + " in our bakery" )
+// 	}
+// }
+// if(flag){
+
+// }
+
+// else{
+// 	document.write("We are sorry."+ userSearch+ " is "  +"<b>" +"not available "  +"</b>"+ "in our bakery" )
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
